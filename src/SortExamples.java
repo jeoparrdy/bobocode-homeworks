d5386a9d4701d91d916da97e063b4f3a74a59aa2
jeoparrdy <46939348+jeoparrdy@users.noreply.github.com>
2021-10-03 12:29:08 +0300

    Merge sort in parallel mode

--- src/SortExamples.java
+++ src/SortExamples.java
@@ -1,72 +1,74 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SortExamples {
 
     public static void main(String[] args) {
-        int [] array ={12,17,5,6,3,3,19,58,42,78,34,23,66};
-        //divideInpArray(array);
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt();
+        }
         printArray(array);
-        insertionSort(array);
+        ForkJoinPool.commonPool().invoke(new MergeSortThread(array));
         printArray(array);
     }
 
     private static void printArray(int[] array) {
 
         System.out.println();
-        for (int element: array) {
+        for (int element : array) {
             System.out.print(element + " ");
         }
     }
+}
 
-    public static void divideInpArray(int[] array){
-        int length = array.length;
-        if (length<2) {
-            return;
-        }
-        int[] leftSubArray = new int[length/2];
-        int[] rightSubArray = new int[length - length/2];
+class MergeSortThread extends RecursiveAction {
+    private int[] array;
+    private int arrayLength;
 
-        System.arraycopy(array,0,leftSubArray,0,leftSubArray.length);
-        System.arraycopy(array, leftSubArray.length, rightSubArray,0,rightSubArray.length);
-
-        divideInpArray(leftSubArray);
-        divideInpArray(rightSubArray);
-
-        mergeSortedSubArr(leftSubArray,rightSubArray,array);
-
+    public MergeSortThread(int[] array) {
+        this.array = array;
+        this.arrayLength = array.length;
     }
 
-    public static void mergeSortedSubArr(int[] leftSubArray, int[] rightSubArray, int[] result ){
-        int cursorL = 0, cursorR = 0, cursorRes =0;
+    @Override
+    protected void compute() {
+        if (arrayLength < 2) { // exit condition
+            return;
+        }
+        int[] leftSubArray = new int[arrayLength / 2]; // left subarray
+        int[] rightSubArray = new int[arrayLength - arrayLength / 2]; // right subarray
 
+        System.arraycopy(array, 0, leftSubArray, 0, leftSubArray.length); //fills the left
+        System.arraycopy(array, leftSubArray.length, rightSubArray, 0, rightSubArray.length); // fills the right
+
+        var leftTask = new MergeSortThread(leftSubArray);
+        var rightTask = new MergeSortThread(rightSubArray);
+
+        leftTask.fork();
+        rightTask.compute();
+        leftTask.join();
+        mergeSortedSubArr(leftSubArray, rightSubArray, array); // merge all in one
+    }
+
+    public static void mergeSortedSubArr(int[] leftSubArray, int[] rightSubArray, int[] result) {
+        int cursorL = 0, cursorR = 0, cursorRes = 0;
         //it works while sub arrays not empty
-        while (cursorL < leftSubArray.length && cursorR < rightSubArray.length){
-            if (leftSubArray[cursorL] <= rightSubArray[cursorR]){
+        while (cursorL < leftSubArray.length && cursorR < rightSubArray.length) {//
+            if (leftSubArray[cursorL] <= rightSubArray[cursorR]) {
                 result[cursorRes++] = leftSubArray[cursorL++];
             } else {
                 result[cursorRes++] = rightSubArray[cursorR++];
             }
         }
-
         //it works if right sub array is empty, but left isn't yet
-        while (cursorL < leftSubArray.length){
+        while (cursorL < leftSubArray.length) {
             result[cursorRes++] = leftSubArray[cursorL++];
         }
-
         //otherwise
-        while (cursorR < rightSubArray.length){
+        while (cursorR < rightSubArray.length) {
             result[cursorRes++] = rightSubArray[cursorR++];
-        }
-    }
-
-    public static void insertionSort(int[] array){
-
-        for (int i = 1; i < array.length; i++) {
-            int current = array[i];
-            int j = i - 1;
-            while(j >= 0 && current < array[j]){
-                array[j+1] = array[j];
-                j--;
-            }
-            array[j+1] = current;
         }
     }
 }
